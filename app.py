d7a14d1da489e02815d821c75b38e44c027e1245
Jud Taylor <jud.taylor@protonmail.com>
2020-03-18 18:06:16 -0400

    Test POST

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+# import json
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/recommend', methods=['POST'])
+    """Accept only POST requests. For now, simply return
+the entire request."""
+def return_recommendation():
+    return request
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+# @app.route('/')
+# def hello():
+#     return "Hello World!"
+
+
+# @app.route('/<name>')
+# def hello_name(name):
+#     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
